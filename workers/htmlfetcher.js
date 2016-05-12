6473bb2628d9ee54ba9b20ee81af378bd20614d0
lessenadam <lessena@gmail.com>
2016-05-12 12:23:02 -0700

    Implement cron function

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,7 +1,12 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
 var archive = require('../helpers/archive-helpers');
+var Promise = require("bluebird");
 
 
-archive.readListOfUrls(archive.downloadUrls);
+archive.readListOfUrls().
+  then(function(urls) {
+    archive.downloadUrls(urls);
+  })
+  .catch(err => console.log(err)); 
 
